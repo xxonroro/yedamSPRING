beb25e3c1905bbb8a027660aaac1653d2e30717b
admin <sdhan1360@gmail.com>
2024-05-29 18:07:58 +0900

    ..

--- boot01/src/main/java/com/yedam/app/emp/mapper/EmpMapper.java
+++ boot01/src/main/java/com/yedam/app/emp/mapper/EmpMapper.java
@@ -1,5 +1,24 @@
 package com.yedam.app.emp.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.yedam.app.emp.service.EmpVO;
+
+@Mapper
 public interface EmpMapper {
 	// DAO - Data Accept Object (DB에 접근할 때 사용하는 객체)
+	
+	// 전체 조회 - (SELECT 문, 조건 X, 결과가 여러 건.)
+	public List<EmpVO> selectEmpAll();
+	
+	// 단건 조회
+	
+	
+	// 등록
+	
+	// 수정
+	
+	// 삭제
 }
--- boot01/src/test/java/com/yedam/app/Demo1ApplicationTests.java
+++ boot01/src/test/java/com/yedam/app/Demo1ApplicationTests.java
@@ -1,13 +1,26 @@
 package com.yedam.app;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpVO;
 
 @SpringBootTest
 class Demo1ApplicationTests {
-
+	@Autowired
+	EmpMapper empMapper;
+	
 	@Test
-	void contextLoads() {
+	void empAllList() {
+		// 전체 조회
+		List<EmpVO> list = empMapper.selectEmpAll();
+		for(EmpVO empVO : list) {
+			System.out.println(empVO);
+		}
 	}
 
 }
